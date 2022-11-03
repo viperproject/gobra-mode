dffe605ae6e73543aebda6f128e3541c3870b0d3
Dspil <dennisspiliopoylos@gmail.com>
2022-11-03 15:50:59 +0100

    make the repo publishable

--- gobra-mode.el
+++ gobra-mode.el
@@ -1,30 +1,20 @@
-;;; gobra-mode.el --- Syntax highlighting for Viper -*- lexical-binding: t -*-
+;;; gobra-mode.el --- Support for Gobra in Emacs -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021 Dionisios Spiliopoulos
+;; Copyright (c) 2022- ETH Zurich.
 
-;; Author: Dionisios Spiliopoulos <dennisspiliopoylos@gmail.com>
 ;; Keywords: lisp
 ;; Version: 0.0.1
-;; URL: https://github.com/Dspil/gobra-mode
+;; URL: https://github.com/viperproject/gobra-mode
 ;; Package-Requires: ((emacs "26.2"))
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 ;;; Commentary:
 
-;; Defines syntax highlighting for the
-;; Gobra language.
+;; Adds syntax highlighting for Gobra and interaction capabilities
+;; with the Gobra executable.
 
 ;;; Code:
 (defvar-local gobra-async-buffer nil "Keeps the async buffer in which gobra runs.")
