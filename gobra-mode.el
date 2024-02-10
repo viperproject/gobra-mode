2b3e1a7375c527acb8a58df9a5d9ff411feb3256
dnezam <55559979+dnezam@users.noreply.github.com>
2024-02-10 14:47:38 +0100

    Add opaque

--- gobra-mode.el
+++ gobra-mode.el
@@ -475,6 +475,7 @@
                          "ensures"
                          "preserves"
                          "trusted"
+			 "opaque"
                          "pred"
                          "pure"
                          "forall"
